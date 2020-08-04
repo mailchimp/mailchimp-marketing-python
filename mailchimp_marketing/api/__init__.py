b69fe340c4441ec2bd78ac7a3d97753e14212163
GitHub Action <action@github.com>
2020-08-04 14:46:23 +0000

    Update mailchimp-marketing-python to v3.0.10

---
+++ mailchimp_marketing/api/__init__.py
@@ -0,0 +1,31 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from mailchimp_marketing.api.activity_feed_api import ActivityFeedApi
+from mailchimp_marketing.api.authorized_apps_api import AuthorizedAppsApi
+from mailchimp_marketing.api.automations_api import AutomationsApi
+from mailchimp_marketing.api.batch_webhooks_api import BatchWebhooksApi
+from mailchimp_marketing.api.batches_api import BatchesApi
+from mailchimp_marketing.api.campaign_folders_api import CampaignFoldersApi
+from mailchimp_marketing.api.campaigns_api import CampaignsApi
+from mailchimp_marketing.api.connected_sites_api import ConnectedSitesApi
+from mailchimp_marketing.api.conversations_api import ConversationsApi
+from mailchimp_marketing.api.dashboard_api import DashboardApi
+from mailchimp_marketing.api.ecommerce_api import EcommerceApi
+from mailchimp_marketing.api.external_auths_api import ExternalAuthsApi
+from mailchimp_marketing.api.facebook_ads_api import FacebookAdsApi
+from mailchimp_marketing.api.file_manager_api import FileManagerApi
+from mailchimp_marketing.api.landing_pages_api import LandingPagesApi
+from mailchimp_marketing.api.lists_api import ListsApi
+from mailchimp_marketing.api.ping_api import PingApi
+from mailchimp_marketing.api.postcards_api import PostcardsApi
+from mailchimp_marketing.api.reporting_api import ReportingApi
+from mailchimp_marketing.api.reports_api import ReportsApi
+from mailchimp_marketing.api.root_api import RootApi
+from mailchimp_marketing.api.search_campaigns_api import SearchCampaignsApi
+from mailchimp_marketing.api.search_members_api import SearchMembersApi
+from mailchimp_marketing.api.template_folders_api import TemplateFoldersApi
+from mailchimp_marketing.api.templates_api import TemplatesApi
+from mailchimp_marketing.api.verified_domains_api import VerifiedDomainsApi
